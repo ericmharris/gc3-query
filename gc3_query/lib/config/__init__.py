e9160923a476d58c394ef4e900a93b886006a6ea
Eric Harris <eric.harris@oracle.com>
2018-04-12 11:46:22 -0700

    lib.config now checkes and reads central secrets.py file

--- gc3_query/lib/config/__init__.py
+++ gc3_query/lib/config/__init__.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
+from gc3_query.lib import *
+from gc3_query.lib.logging import Logging, get_logging
 
+_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
+
+secrets_file = BASE_DIR.joinpath('etc/config/secrets.py')
+
+if secrets_file.exists():
+    from gc3_query.etc.config.secrets import opc_password
+else:
+    raise RuntimeError(f'File Not Found: {secrets_file}\n')
