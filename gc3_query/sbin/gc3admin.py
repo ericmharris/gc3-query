531288128d149e39ca61ab25133762494c8945bd
Eric Harris <eric.harris@oracle.com>
2018-05-08 08:52:31 -0700

    created gc3admin

---
+++ gc3_query/sbin/gc3admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for gc3admin."""
+import sys
+import click
+
+from gc3_query.lib import *
+from gc3_query.lib.gc3admincli import cli
+from gc3_query.lib.logging import get_logging
+from gc3_query.lib.gc3adminlib import *
+from gc3_query.lib.cli.gc3admin import *
+
+_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
+
+if __name__ == "__main__":
+    try:
+        cli()
+    except KeyboardInterrupt:
+        print(f'Caught KeyboardInterrupt, shutting down.')
+        sys.exit(0)
