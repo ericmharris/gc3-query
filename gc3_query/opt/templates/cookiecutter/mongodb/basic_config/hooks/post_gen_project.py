4cd20d004b1c0e947b7bd23366d0e6054eaf9555
Eric Harris <eric.harris@oracle.com>
2018-05-08 08:37:41 -0700

    template to setup mongodb

--- gc3_query/opt/templates/cookiecutter/mongodb/basic_config/hooks/post_gen_project.py
+++ gc3_query/opt/templates/cookiecutter/mongodb/basic_config/hooks/post_gen_project.py
@@ -1,23 +1,46 @@
 import os
-import sys
 
 
 def main():
-    sys.exit(clean_unused_color_files())
+    favorite_color = '{{cookiecutter.favorite_color}}'
+    remove_unused_project_files(favorite_color)
+    display_getting_started_instructions(favorite_color)
 
 
-def clean_unused_color_files():
-    selected_game = '{{ cookiecutter.game_type }}'
-    working = os.path.abspath(os.path.join(os.path.curdir))
-    files = ["hilo.py", "pacman.py", "pong.py"]
-    for file in files:
-        if selected_game + ".py" != file:
-            os.remove(os.path.join(working, file))
+# Selected project files based on user choices
+def remove_unused_project_files(favorite_color):
+    working_dir = os.path.join(
+        os.path.abspath(os.path.curdir), 'color_files')
 
-    full_selected = os.path.join(working, selected_game + ".py")
-    game = os.path.join(working, 'game.py')
-    os.rename(full_selected, game)
-    return 0
+    file_list = []
+    if favorite_color == 'yellow':
+        file_list = ['blue_file.txt', 'green_file.txt']
+    if favorite_color == 'green':
+        file_list = ['blue_file.txt', 'yellow_file.txt']
+    if favorite_color == 'blue':
+        file_list = ['green_file.txt', 'yellow_file.txt']
+
+    delete_files(file_list, working_dir)
+
+
+def delete_files(file_list, working_dir):
+    for file in file_list:
+        full_file = os.path.join(working_dir, file)
+        if os.path.exists(full_file):
+            os.remove(full_file)
+        else:
+            print("WARNING: Could not file to remove in hook: " + full_file)
+
+
+def display_getting_started_instructions(favorite_color):
+    print()
+    print("------------------------------------------")
+    print("  Welcome to the color project: " + favorite_color)
+    print("  Your world is a rainbow (but mostly " + favorite_color + ")")
+    print("------------------------------------------")
+    print()
+    print("To get started, read the readme.md")
+    print()
 
 
 if __name__ == '__main__':
