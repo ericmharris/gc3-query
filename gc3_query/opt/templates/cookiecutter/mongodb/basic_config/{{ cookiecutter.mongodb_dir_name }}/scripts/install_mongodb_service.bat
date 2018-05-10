feb6c4319ccb3643e1c1e3b3864e7deca4cb9f7e
Eric Harris <eric.harris@oracle.com>
2018-05-10 16:01:56 -0700

    updated service config scripts

--- gc3_query/opt/templates/cookiecutter/mongodb/basic_config/{{ cookiecutter.mongodb_dir_name }}/scripts/install_mongodb_service.bat
+++ gc3_query/opt/templates/cookiecutter/mongodb/basic_config/{{ cookiecutter.mongodb_dir_name }}/scripts/install_mongodb_service.bat
@@ -1,6 +1,10 @@
 @echo off
 echo Installing MongoDB service
 REM -- -"C:\tools\MongoDB\Server\3.6\bin\mongod.exe" --config C:\tools\MongoDB\configs\mongo-service.config --install
-"{{ cookiecutter.mongod_bin }}" --config "{{ cookiecutter.mongodb_service_config_file }}" --install
+"{{ cookiecutter.mongod_bin }}" --config "{{ cookiecutter.mongodb_service_config_file }}" --install --serviceName MongoDB
 echo MongoDB service installed.
+echo Configuring MongoDB service for Manual startup-type
+sc config "MongoDB" start=demand
+echo MongoDB service configured for Manual startup
+echo done.
 pause
--- gc3_query/opt/templates/cookiecutter/mongodb/basic_config/{{ cookiecutter.mongodb_dir_name }}/scripts/remove_mongodb_service.bat
+++ gc3_query/opt/templates/cookiecutter/mongodb/basic_config/{{ cookiecutter.mongodb_dir_name }}/scripts/remove_mongodb_service.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Installing MongoDB service
 REM -- -"C:\tools\MongoDB\Server\3.6\bin\mongod.exe" --config C:\tools\MongoDB\configs\mongo-service.config --install
-"{{ cookiecutter.mongod_bin }}" --config "{{ cookiecutter.mongodb_service_config_file }}" --remove
-echo MongoDB service installed.
+"{{ cookiecutter.mongod_bin }}" --config "{{ cookiecutter.mongodb_service_config_file }}" --remove --serviceName MongoDB
+echo MongoDB service removed.
+echo done.
 pause
