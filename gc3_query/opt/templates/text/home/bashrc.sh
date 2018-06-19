a1ae0e9a3af02a90cd0ae40434b0edda6bdd256d
Eric Harris <eric.harris@oracle.com>
2018-06-19 11:35:35 -0700

    added cloud stack templates and bashrc

---
+++ gc3_query/opt/templates/text/home/bashrc.sh
@@ -0,0 +1,47 @@
+# .bashrc
+################################################################################
+###  Bash RC File
+################################################################################
+# 0644 permissions
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
+# User specific aliases and functions
+set -o vi
+export GREP_OPTIONS='--color=auto'
+
+alias sd='sudo su - '
+
+alias ll='ls -l'
+alias llh='ls -lh'
+
+
+alias gadd='git add -A'
+alias gcomm='git commit -a'
+alias gstat='git status'
+alias gpull='git pull origin master'
+alias gpush='git push origin master'
+alias gpush='git push origin master'
+
+timestamp ()
+{
+# 04_25_2018.15_55_28
+	# date +%m_%d_%Y.%H_%M_%S
+	# date +%Y%m%dT%H%M%S
+	# 2018_06_19T132208
+	# date +%Y_%m_%dT%H%M%S
+	# 2018_06_19T13.22.35
+	date +%Y_%m_%dT%H.%M.%S
+}
+
+datestamp ()
+{
+# 04_25_2018
+	date +%m_%d_%Y
+}
+
+alias ts='timestamp'
+alias ds='datestamp'
