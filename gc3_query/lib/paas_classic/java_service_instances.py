8e72b0762b27bf9ecf7143a4b91d43e37ae5c7d9
Eric Harris <eric.harris@oracle.com>
2018-08-24 10:51:55 -0700

    DatabaseServiceInstances works

---
+++ gc3_query/lib/paas_classic/java_service_instances.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+gc3-query.java_service_instances    [8/24/2018 10:20 AM]
+~~~~~~~~~~~~~~~~
+
+<DESCR SHORT>
+
+<DESCR>
+"""
+
+################################################################################
+## Standard Library Imports
+import sys, os
+
+################################################################################
+## Third-Party Imports
+from dataclasses import dataclass
+
+################################################################################
+## Project Imports
+from gc3_query.lib import *
+from . import *
+
+_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
+
+class JavaServiceInstances(PaaSServiceBase):
+
+    def __init__(self,
+                 service_cfg: NestedOrderedDictAttrListBase,
+                 idm_cfg: NestedOrderedDictAttrListBase,
+                 http_client: Union[IaaSRequestsHTTPClient, None] = None,
+                 from_url: Optional[bool] = False,
+                 storage_delegates: Optional[List[str]] = None,
+                 **kwargs: DictStrAny):
+        super().__init__(service_cfg=service_cfg, idm_cfg=idm_cfg, http_client=http_client, from_url=from_url, storage_delegates=storage_delegates, **kwargs)
+        _debug(f"{self.name} created")
+
+    def get_all_domain_data(self):
+        """
+
+        :return:
+        """
+        http_future = self.service_operations.get_domain(identityDomainId=self.idm_cfg.name)
+        service_response: BravadoResponse = http_future.response()
+        result = service_response.result
+        metadata: BravadoResponseMetadata = service_response.metadata
+        domains = result['services']
+        return domains
