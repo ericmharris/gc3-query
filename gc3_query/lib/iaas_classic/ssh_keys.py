aecc741d45bafd3214d3be38ff9c79c600228ccd
Eric Harris <ericmharris@gmail.com>
2018-08-27 11:06:12 -0700

    asdf

--- gc3_query/lib/iaas_classic/ssh_keys.py
+++ gc3_query/lib/iaas_classic/ssh_keys.py
@@ -43,5 +43,12 @@ class SSHKeys(IaaSServiceBase):
 
 
 
-from gc3_query.lib import get_logging
-_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
+    def get_all_ssh_keys(self):
+        http_future = self.service_operations.list_ssh_key(container=self.idm_root_container_name)
+        # http_future = self.bravado_service_operations.listSecRule(container=self.idm_root_container_name)
+        request_url = http_future.future.request.url
+        service_response = http_future.response()
+        result_json = service_response.incoming_response.json()
+        return result_json
+
+
