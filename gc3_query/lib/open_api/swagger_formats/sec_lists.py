9672130637fe2298e5e6d3b7e2173460daa074b4
Eric Harris <eric.harris@oracle.com>
2018-09-10 11:31:54 -0700

    SecListFormats work

--- gc3_query/lib/open_api/swagger_formats/sec_lists.py
+++ gc3_query/lib/open_api/swagger_formats/sec_lists.py
@@ -30,6 +30,8 @@ _debug, _info, _warning, _error, _critical = get_logging(name=__name__)
 
 class SecListBaseFormat:
 
+    idm_instance_id_to_name = {idm_domain.service_instance_id:idm_domain.name for idm_domain in [d for d in gc3_cfg.idm.domains.values() if not isinstance(d, str)]}
+
 
     def __init__(self, name: str):
         """Takes three-part name of object from Oracle Cloud Classic.
@@ -44,26 +46,36 @@ class SecListBaseFormat:
         seclist:/Compute-586297329/sharad.salian@oracle.com/dbaas/gc3emeaczsi201/db_1/ora_db
         seclist:/Compute-586297329/sharad.salian@oracle.com/paas/SOA/gc3emeaoics106/lb/ora_otd_infraadmin
 
+
+
         :param name:  The three-part name of the object (/Compute-identity_domain/user/object).
         """
-        self._name = name
-        self.identitiy_domain, user, object_name = self._parse_name(name=name)
+        self.name = name.strip()
+        # self.idm_service_instance_id, self.username, self.list_object_name, self.list_type = self._parse_name(name=self.name)
+        # self.idm_domain_name = self.idm_instance_id_to_name.get(self.idm_service_instance_id, False)
+        # if not self.idm_domain_name:
+        #     raise RuntimeError(f"Failed to parse IDM Domain name for {self.__class__.__name__}: {name}")
+        # _debug(f"{self.__class__.__name__} created ")
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self})"
 
 
-    def _parse_name(self, name: str)->Tuple[str]:
-        _, _name = name.split(':')
-        idm_part, user, *obejct_names = _name.split('/')
-        obejct_name = '/'.join(obejct_names)
-        return idm_part, user, obejct_name
+    def to_wire(self):
+        return self.__str__()
+
+
 
     @classmethod
-    def validate(cls, from_wire: str) -> bool:
-        _debug(f"from_wire={from_wire}")
-        try:
-            as_boolean = cls.str_to_bool[from_wire.lower()]
-        except KeyError:
-            raise SwaggerValidationError(f"Value={from_wire} not recognized as SecListFormat")
-        return isinstance(as_boolean, bool)
+    def validate(cls, sec_list_name: str) -> bool:
+        if 'seclist' in sec_list_name:
+            return True
+        if 'seciplist' in sec_list_name:
+            return True
+        raise SwaggerValidationError(f"Value={from_wire} not recognized as SecListFormat")
 
 
 
@@ -80,8 +92,24 @@ class SecListFormat(SecListBaseFormat):
 
         """
         super().__init__(name)
+        self.idm_service_instance_id, self.username, self.list_object_name, self.list_type = self._parse_name(name=self.name)
+        self.idm_domain_name = self.idm_instance_id_to_name.get(self.idm_service_instance_id, False)
+        if not self.idm_domain_name:
+            raise RuntimeError(f"Failed to parse IDM Domain name for {self.__class__.__name__}: {name}")
+        _debug(f"{self.__class__.__name__} created ")
 
 
+    def _parse_name(self, name: str)->Tuple[str]:
+        list_type, _name = name.split(':')
+        _, idm_name_part, username, *object_name_parts = _name.split('/')
+
+        # idm_name_part = 'Compute-605519274'
+        _, _idm_service_instance_id = idm_name_part.split('-')
+        idm_service_instance_id = int(_idm_service_instance_id)
+
+        _list_object_name = '/'.join(object_name_parts)
+        list_object_name = f"/{_list_object_name}"
+        return idm_service_instance_id, username, list_object_name, list_type
 
 
 
@@ -98,16 +126,25 @@ class SecIPListFormat(SecListBaseFormat):
 
         """
         super().__init__(name)
+        self.list_object_name, self.list_type = self._parse_name(name=self.name)
+        _debug(f"{self.__class__.__name__} created ")
 
+
+    def _parse_name(self, name: str)->Tuple[str]:
+        list_type, list_object_name = name.split(':')
+        return  list_object_name, list_type
 
 
 
 def from_wire(name: str) -> Union[SecListFormat, SecIPListFormat]:
-    if name.startswith('seclist'):
+    if 'seclist' in name:
         return SecListFormat(name=name)
-    if name.startswith('seciplist'):
+    if 'seciplist' in name:
         return SecIPListFormat(name=name)
     raise SwaggerValidationError(f'Failed to find SecList/SecIPList format in: {name}')
+
+
+
 
 sec_list_format = SwaggerFormat(
     # name of the format as used in the Swagger spec
@@ -115,12 +152,12 @@ sec_list_format = SwaggerFormat(
 
     # Callable to convert a python object to_wire representations
     # to_wire=lambda json_bool_instance: json_bool_instance.as_wire,
-    to_wire=from_wire,
+    to_wire=lambda sec_list_format_instance: sec_list_format_instance.to_wire,
 
     # Callable to convert a from_wire to a python object
-    to_python=lambda s: SecListBaseFormat(s),
+    to_python=from_wire,
 
     # Callable to validate the cidr in string form
     validate=SecListBaseFormat.validate,
-    description='Converts "true" and "false" to/from equivalent booleans.'
+    description='Converts seciplist:/oracle/public/paas-infra or seclist:/Compute-605519274/siva.subramani@oracle.com/paas/JaaS/gc3ossitcmf103JAAS/wls/ora_ms into objects'
 )
--- tests/open_api/swagger_formats/sec_list/test_sec_list_format.py
+++ tests/open_api/swagger_formats/sec_list/test_sec_list_format.py
@@ -15,60 +15,25 @@ import sys, os
 
 ################################################################################
 ## Third-Party Imports
-from dataclasses import dataclass
-
-################################################################################
-## Project Imports
-from gc3_query.lib import *
-
-_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
-
-
-###############################################################################
-## Standard Library Imports
-import sys, os
-
-################################################################################
-## Third-Party Imports
-from dataclasses import dataclass
-
-################################################################################
-## Project Imports
-from gc3_query.lib import *
-from gc3_query.lib.iaas_classic.models.sec_list_model import SecListModel
-
-_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
-
-
-from pathlib import Path
-
 import pytest
-from mongoengine import connect
+from dataclasses import dataclass
+from bravado_core.formatter import SwaggerFormat, NO_OP
+from bravado_core.exception import SwaggerValidationError
+
+################################################################################
+## Project Imports
+from gc3_query.lib import *
+from gc3_query.lib.open_api.swagger_formats.sec_lists import from_wire, SecListBaseFormat, SecListFormat, SecIPListFormat
 
 from gc3_query.lib import gc3_cfg
 from gc3_query.lib.gc3_config import GC3Config
 from gc3_query.lib.iaas_classic.models.sec_list_model import SecListModel
 from gc3_query.lib.iaas_classic.sec_lists import SecLists
-from gc3_query.lib.base_collections import NestedOrderedDictAttrListBase
 # fixme? from gc3_query.lib.open_api import API_SPECS_DIR
 import json
 from pathlib import Path
 from pymongo import MongoClient
-import pytest
-from bravado_core.spec import Spec
-from bravado.response import  BravadoResponse, BravadoResponseMetadata
-import mongoengine
-from mongoengine.connection import get_connection, register_connection
-from gc3_query.lib import *
-from gc3_query.lib import gc3_cfg
-from gc3_query.lib.export_delegates.mongodb import storage_adapter_init
 
-# from gc3_query.lib.export_delegates.mongodb import storage_adapter_init
-# # fixme? from gc3_query.lib.open_api import API_SPECS_DIR
-from pathlib import Path
-from gc3_query.lib import *
-import pytest
-# from pprint import pprint, pformat
 
 _debug, _info, _warning, _error, _critical = get_logging(name=__name__)
 
@@ -86,18 +51,73 @@ def test_setup():
         output_dir.mkdir()
 
 
-inputs = """
-        seclist:/Compute-586297329/siva.subramani@oracle.com/paas/JaaS/gc3emacmma804/lb/ora_otd_infraadmin
-        seclist:/Compute-586297329/siva.subramani@oracle.com/paas/JaaS/gc3emacmma804/wls/ora_wls_infraadmin
-        seclist:/Compute-586297329/mayurnath.gokare@oracle.com/paas/JaaS/gc3apacasbx546/lb/ora_otd
-        seclist:/Compute-586297329/dhiru.vallabhbhai@oracle.com/dbaas/gc3emeacetr001/db_1/ora_db
-        seclist:/Compute-586297329/ramesh.dadhania@oracle.com/paas/ANALYTICS/gc3emeatfde102/BI/ora_BISecList
-        seclist:/Compute-586297329/seetharaman.nandyal@oracle.com/paas/JaaS/gc3apacasbx529/wls/ora_ms
-        seclist:/Compute-586297329/seetharaman.nandyal@oracle.com/paas/JaaS/gc3apacasbx529/lb/ora_otd
-        seclist:/Compute-586297329/sharad.salian@oracle.com/dbaas/gc3emeaczsi201/db_1/ora_db
-        seclist:/Compute-586297329/sharad.salian@oracle.com/paas/SOA/gc3emeaoics106/lb/ora_otd_infraadmin
-                seciplist:/oracle/public/paas-infra
-"""
+
+
+
+@pytest.fixture()
+def setup_seclist_from_wire():
+    gc30003_inputs = """
+    seclist:/Compute-586297329/siva.subramani@oracle.com/paas/JaaS/gc3emacmma804/lb/ora_otd_infraadmin
+    seclist:/Compute-586297329/mayurnath.gokare@oracle.com/paas/JaaS/gc3apacasbx546/lb/ora_otd
+    seclist:/Compute-586297329/dhiru.vallabhbhai@oracle.com/dbaas/gc3emeacetr001/db_1/ora_db
+    seclist:/Compute-586297329/ramesh.dadhania@oracle.com/paas/ANALYTICS/gc3emeatfde102/BI/ora_BISecList
+    seclist:/Compute-586297329/seetharaman.nandyal@oracle.com/paas/JaaS/gc3apacasbx529/wls/ora_ms
+    seclist:/Compute-586297329/sharad.salian@oracle.com/dbaas/gc3emeaczsi201/db_1/ora_db
+    seciplist:/oracle/public/paas-infra
+    """.splitlines()
+    gc30003_inputs = [s.strip() for s in gc30003_inputs if 'seclist' in s or 'seciplist' in s]
+
+    gc35001_inputs = """
+    seclist:/Compute-605519274/ramesh.dadhania@oracle.com/paas/OEHPCS/gc3ntag401OEHPCS/kafka/ora_kaf_infraadmin
+    seclist:/Compute-605519274/siva.subramani@oracle.com/paas/IOTEnterpriseApps/gc3ossitcmf103IOT/Nosql/ora_NosqlSecList
+    seclist:/Compute-605519274/siva.subramani@oracle.com/paas/JaaS/gc3ossitcmf103JAAS/wls/ora_ms
+    seclist:/Compute-605519274/sharad.salian@oracle.com/paas/IOTEnterpriseApps/gc3ossiducm203IOT/ES/ora_ESSecList
+    seciplist:/oracle/public/paas-infra
+    """.splitlines()
+    gc35001_inputs = [s.strip() for s in gc35001_inputs if 'seclist' in s or 'seciplist' in s]
+
+    idm_instance_id_to_name = {idm_domain.service_instance_id: idm_domain.name for idm_domain in [d for d in gc3_cfg.idm.domains.values() if not isinstance(d, str)]}
+    idm_name_to_instance_id = {idm_domain.name:idm_domain.service_instance_id for idm_domain in [d for d in gc3_cfg.idm.domains.values() if not isinstance(d, str)]}
+
+    assert gc30003_inputs
+    yield gc30003_inputs, gc35001_inputs, idm_instance_id_to_name, idm_name_to_instance_id
+
+
+def test_first_gc30003_seclist(setup_seclist_from_wire):
+    gc30003_inputs, gc35001_inputs, idm_instance_id_to_name, idm_name_to_instance_id = setup_seclist_from_wire
+    first_gc30003 = gc30003_inputs[0]
+    first_sec_list_fmt = from_wire(first_gc30003)
+    assert first_sec_list_fmt
+    assert first_sec_list_fmt.idm_domain_name in idm_name_to_instance_id
+    assert first_sec_list_fmt.idm_service_instance_id in idm_instance_id_to_name
+    assert first_sec_list_fmt.list_type=='seclist'
+
+
+def test_first_gc35001_seclist(setup_seclist_from_wire):
+    gc35001_inputs, gc35001_inputs, idm_instance_id_to_name, idm_name_to_instance_id = setup_seclist_from_wire
+    first_gc35001 = gc35001_inputs[0]
+    sec_list_fmt = from_wire(first_gc35001)
+    assert sec_list_fmt
+    assert sec_list_fmt.idm_domain_name in idm_name_to_instance_id
+    assert sec_list_fmt.idm_service_instance_id in idm_instance_id_to_name
+
+
+def test_first_gc30003_seciplist_from_wire(setup_seclist_from_wire):
+    gc30003_inputs, gc35001_inputs, idm_instance_id_to_name, idm_name_to_instance_id = setup_seclist_from_wire
+    last_gc30003 = gc30003_inputs[-1]
+    last_sec_list_fmt = from_wire(last_gc30003)
+    assert last_sec_list_fmt
+    assert last_sec_list_fmt.list_type=='seciplist'
+
+
+
+
+
+
+
+
+
+
 
 @pytest.fixture()
 def setup_gc30003_format():
