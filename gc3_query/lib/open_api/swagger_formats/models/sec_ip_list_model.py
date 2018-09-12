58e8fbc6e6be79acf7beabf6b88281feb3fd1fde
Eric Harris <eric.harris@oracle.com>
2018-09-12 12:54:51 -0700

    more work with embededs

---
+++ gc3_query/lib/open_api/swagger_formats/models/sec_ip_list_model.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+gc3-query.sec_ip_list_model    [9/12/2018 12:25 PM]
+~~~~~~~~~~~~~~~~
+
+<DESCR SHORT>
+
+<DESCR>
+"""
+
+################################################################################
+## Standard Library Imports
+import sys, os
+
+################################################################################
+## Third-Party Imports
+from dataclasses import dataclass
+
+################################################################################
+## Project Imports
+from gc3_query.lib import *
+from . import *
+_debug, _info, _warning, _error, _critical = get_logging(name=__name__)
+
+
+
+class SecIPListModel(EmbeddedDocument):
+    """
+    """
+
+    idm_service_instance_id = StringField()
+    object_owner = StringField()
+    object_name = StringField()
+    object_type = StringField()
+    idm_domain_name = StringField()
+
+    def __init__(self, *args, **values):
+        super().__init__(*args, **values)
+        _debug(f"{self.__class__.__name__}.__init__(args={args}, values={values}):")
